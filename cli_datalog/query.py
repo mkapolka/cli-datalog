d2cfa21b3548008e971f7dba7111008189f5e83d
Marek Kapolka <mkapolka@twitch.tv>
2019-03-27 13:59:16 -0700

    New method for doing csv input

--- cli_datalog/query.py
+++ cli_datalog/query.py
@@ -164,10 +164,27 @@ def print_op(neg, df, *args, **kwargs):
     print(df)
     return df
 
+def csv_input_op(neg, df, filename, *args, **kwargs):
+    if neg:
+        raise Exception("Negation not supported for csv.read")
+    filename = filename.value()
+
+    # constant values
+    const_df = pd.DataFrame([{i: a.value() for i, a in enumerate(args) if not a.is_var()}])
+    const_df = const_df.assign(key=0)
+
+    file_df = pd.read_csv(filename, header=None, dtype=str)
+    file_df = file_df.assign(key=0)
+
+    file_df = file_df.merge(const_df, how="inner")
+    file_df = file_df.rename(columns={i: a.value() for i, a in enumerate(args) if a.is_var()})
+    return df.merge(file_df, how="inner")
+
 DEFAULT_OPERATORS = {
     "print": print_op,
     "eq": filter_op(lambda x, y: x == y),
     "neq": filter_op(lambda x, y: x == y),
     "gt": filter_op(lambda x, y: float(x) > float(y)),
     "csv": csv_op,
+    "csv.read": csv_input_op,
 }
